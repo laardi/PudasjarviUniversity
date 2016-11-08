136d3056140a0daeb69c23243218adda5c3ac5e5
laardi <laardi@gmail.com>
2016-11-08 14:52:40 +0200

    a
    a

--- js/stuff.js
+++ js/stuff.js
@@ -2,45 +2,45 @@
 
 
 $( document ).ready(function() {
-    $('.datepicker').datepicker();
+    //$('.datepicker').datepicker();
     console.log("ready, ime sykkivää");
     
-    $( '#hinttari' ).click( function(event) {
-        var formData = $( '#varaus-form' ).serialize();
-        //e.preventDefault();
-        //alert("DSFFD");
-        //console.log("BÖH") ;
-        //console.log(formData);
-        var ding = formData.split("&");
-        //console.log(ding);
-        var tempArray = {} ;
-        for ( i=0; i < ding.length; i++) {
-            //console.log(ding[i]);
-            var doodle = ding[i].split("=");
-            //console.log(doodle);
-            tempArray[doodle[0]] = doodle[1];
-        }
-        
-        console.log(tempArray["alakupvm"]);
-        console.log(tempArray["loppupvm"]);
-        console.log(tempArray["user"]);
-        console.log(tempArray["room"]);
-        
-        var dataToSend =    {"template":
-                                {"data":
-                                    [
-                                    {"prompt":"","name":"user","value":""},
-                                    {"prompt":"","name":"rdate","value":""},
-                                    {"prompt":"","name":"ldate","value":""},
-                                    {"prompt":"","name":"item","value":""},
-                                    ]
-                                }
-                            };
-        
-        
-        
-        return false;
-    });
+    //$( '#hinttari' ).click( function(event) {
+    //    var formData = $( '#varaus-form' ).serialize();
+    //    //e.preventDefault();
+    //    //alert("DSFFD");
+    //    //console.log("BÖH") ;
+    //    //console.log(formData);
+    //    var ding = formData.split("&");
+    //    //console.log(ding);
+    //    var tempArray = {} ;
+    //    for ( i=0; i < ding.length; i++) {
+    //        //console.log(ding[i]);
+    //        var doodle = ding[i].split("=");
+    //        //console.log(doodle);
+    //        tempArray[doodle[0]] = doodle[1];
+    //    }
+    //    
+    //    console.log(tempArray["alakupvm"]);
+    //    console.log(tempArray["loppupvm"]);
+    //    console.log(tempArray["user"]);
+    //    console.log(tempArray["room"]);
+    //    
+    //    var dataToSend =    {"template":
+    //                            {"data":
+    //                                [
+    //                                {"prompt":"","name":"user","value":""},
+    //                                {"prompt":"","name":"rdate","value":""},
+    //                                {"prompt":"","name":"ldate","value":""},
+    //                                {"prompt":"","name":"item","value":""},
+    //                                ]
+    //                            }
+    //                        };
+    //    
+    //    
+    //    
+    //    return false;
+    //});
 });
 
 
